a96537a84fa096567abfd792897bcda70bcd6ce8
AbhishekJ24 <abhishekjoshiofficial24@gmail.com>
2023-11-18 12:20:28 +0530

    removed redundant files

--- kadanessubarraysum.java
+++
@@ -1,5 +0,0 @@
-public class kadanessubarraysum {
-        public static void main(String[] args) {
-            //int arr[]={-2,-3,4,-1,-2,1,5,-3};
-        }
-}
--- oddeven.java
+++
@@ -1,9 +0,0 @@
-public class oddeven{
-    public static void main(String[] args) {
-        int num=;
-        if( (num&1) ==1)
-        System.out.println("odd");
-        else
-        System.out.println("even");
-    }
-}
