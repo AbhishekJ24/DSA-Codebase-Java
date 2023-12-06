6c73f5523881f43235642b08526cde777bf4e4d2
AbhishekJ24 <abhishekjoshiofficial24@gmail.com>
2023-12-07 02:14:33 +0530

    permutations - backtracking added

---
+++ Backtracking/Permutations_Backtracking.java
@@ -0,0 +1,20 @@
+package Backtracking;
+
+public class Permutations_Backtracking {
+    public static void perm(String str, String ans) {
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+        for(int i=0;i<str.length();i++){
+            char current=str.charAt(i);
+            String newStr=str.substring(0,i)+str.substring(i+1);
+            perm(newStr,ans+current);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abcd";
+        perm(str, "");
+    }
+}
