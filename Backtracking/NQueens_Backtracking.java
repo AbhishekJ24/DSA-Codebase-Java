b06c36f2a94a83a8c255fbb9689f436192412ed7
AbhishekJ24 <abhishekjoshiofficial24@gmail.com>
2023-12-08 00:16:02 +0530

    nqueens - backtracking added

---
+++ Backtracking/NQueens_Backtracking.java
@@ -0,0 +1,55 @@
+package Backtracking;
+
+public class NQueens_Backtracking {
+    public static boolean isSafe(char ch[][], int row, int col){
+        //vertically upwards
+        for(int k=row-1;k>=0;--k){
+            if(ch[k][col]=='Q')
+            return false;
+        }
+        //left diagonally upwards
+        for(int k=row-1, kk=col-1;k>=0 && kk>=0;--k,--kk){
+            if(ch[k][kk]=='Q')
+            return false;
+        }
+        //right diagonally upwards
+        for(int k=row-1, kk=col+1;k>=0 && kk<ch.length;--k,++kk){
+            if(ch[k][kk]=='Q')
+            return false;
+        }
+        return true;
+    }
+    public static void nQueens(char ch[][],int row){
+        if(row==ch.length){
+            printBoard(ch);
+            return;
+        }
+        for (int col = 0; col < ch.length; col++) {
+            if(isSafe(ch,row,col)){
+                ch[row][col]='Q';
+                nQueens(ch,row+1);
+                ch[row][col]='X';
+            }
+        }
+    }
+    public static void printBoard(char ch[][]) {
+        System.out.println("\n---------- CHESS  BOARD ----------\n");
+        for (int i = 0; i < ch.length; i++) {
+            for (int j = 0; j < ch.length; j++) {
+                System.out.print(ch[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        char ch[][] = new char[n][n];
+        for (int i = 0; i < ch.length; i++) {
+            for (int j = 0; j < ch.length; j++) {
+                ch[i][j]='X';
+            }
+        }
+        nQueens(ch, 0);
+    }
+}
