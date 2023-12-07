7830fbe6ea71f5e5a949a871a332b7c5b8d659e9
AbhishekJ24 <abhishekjoshiofficial24@gmail.com>
2023-12-07 18:32:21 +0530

    formatting

--- Backtracking/Permutations_Backtracking.java
+++ Backtracking/Permutations_Backtracking.java
@@ -6,10 +6,10 @@ public class Permutations_Backtracking {
             System.out.println(ans);
             return;
         }
-        for(int i=0;i<str.length();i++){
-            char current=str.charAt(i);
-            String newStr=str.substring(0,i)+str.substring(i+1);
-            perm(newStr,ans+current);
+        for (int i = 0; i < str.length(); i++) {
+            char current = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+            perm(newStr, ans + current);
         }
     }
 
--- Backtracking/Subsets_Backtracking.java
+++ Backtracking/Subsets_Backtracking.java
@@ -1,19 +1,21 @@
 package Backtracking;
-public class Subsets_Backtracking{
-    public static void findSubsets(String str, int i, String ans){
-        if(i==str.length()){
-            if(ans.length()==0)
-            System.out.println("null");
+
+public class Subsets_Backtracking {
+    public static void findSubsets(String str, int i, String ans) {
+        if (i == str.length()) {
+            if (ans.length() == 0)
+                System.out.println("null");
             else
-            System.out.println(ans);
+                System.out.println(ans);
             return;
         }
-        findSubsets(str,i+1,ans+str.charAt(i));
-        findSubsets(str,i+1,ans);
+        findSubsets(str, i + 1, ans + str.charAt(i));
+        findSubsets(str, i + 1, ans);
     }
+
     public static void main(String[] args) {
-        String str="xyz";
-        findSubsets(str,0,"");
+        String str = "xyz";
+        findSubsets(str, 0, "");
     }
 
 }
