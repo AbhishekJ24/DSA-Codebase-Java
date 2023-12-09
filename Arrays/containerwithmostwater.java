276f24799eed6c231415fdd92f8c932381b271a4
AbhishekJ24 <abhishekjoshiofficial24@gmail.com>
2023-12-09 15:31:15 +0530

    cwmw added in arrays

---
+++ Arrays/containerwithmostwater.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class containerwithmostwater {
+    public int maxArea(int[] height) {
+        int maxWater = Integer.MIN_VALUE;
+        int lp = 0, rp = height.length - 1;
+        while (lp < rp) {
+            int width = 1 * Math.abs(lp - rp);
+            int heig = Math.min(height[lp], height[rp]);
+            int max = width * heig;
+            if (maxWater < max)
+                maxWater = max;
+            if (height[lp] < height[rp])
+                lp++;
+            else
+                rp--;
+        }
+        return maxWater;
+    }
+}
