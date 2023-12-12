c277ac0cffb5b897b91a89b74fd27f579c8d20b9
AbhishekJ24 <abhishekjoshiofficial24@gmail.com>
2023-12-12 14:19:00 +0530

    leetcode daily - max product of two elements in an array added

---
+++ Arrays/maxprodoftwoelementsinarray.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class maxprodoftwoelementsinarray {
+    public static int maxProduct(int[] nums) {
+        int lp=0;
+        int rp=nums.length-1;
+        int max=Integer.MIN_VALUE;
+        while(lp<rp){
+            int value=(nums[lp]-1)*(nums[rp]-1);
+            max=Math.max(max,value);
+            if(nums[lp]<nums[rp])
+            lp++;
+            else
+            rp--;
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int nums[]={1,2,3,4,5,6,7,8,9,10};
+        System.out.println(maxProduct(nums));
+    }
+}
