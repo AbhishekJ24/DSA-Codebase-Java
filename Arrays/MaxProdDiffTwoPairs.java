a7ee648639f04882104521d9d986e153c6efc597
AbhishekJ24 <abhishekjoshiofficial24@gmail.com>
2023-12-18 23:08:06 +0530

    Maximum Product Difference Between Two Pairs - Arrays - LeetCode Daily added

---
+++ Arrays/MaxProdDiffTwoPairs.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class MaxProdDiffTwoPairs {
+    public static void main(String[] args) {
+        int nums[]={5,6,2,7,4};
+        Arrays.sort(nums);
+        System.out.println((nums[nums.length - 1] * nums[nums.length - 2]) - (nums[1] * nums[0]));
+    }
+}
