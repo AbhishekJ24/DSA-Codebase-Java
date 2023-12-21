f0ed6ae8831579fd8b365d493ca338c2d8405b09
AbhishekJ24 <abhishekjoshiofficial24@gmail.com>
2023-12-21 18:45:17 +0530

    Widest Vertical Area - Arrays - LeetCode Daily Added

---
+++ Arrays/widestverticalarea.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class widestverticalarea{
+    public static int verticalArea(int points[][]){
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+        int ans = 0;
+        for (int i = 1; i < points.length; i++) {
+            ans = Math.max(ans, points[i][0] - points[i - 1][0]);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int points[][]={{3,1},{9,0},{1,0},{1,4},{5,3},{8,8}};
+        System.out.println(verticalArea(points));
+    }
+}
