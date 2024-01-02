99045ae201a55cfc5ef868f1db2b7c94f1d4cbbb
AbhishekJ24 <abhishekjoshiofficial24@gmail.com>
2024-01-02 22:05:56 +0530

    Reverse Linked List - Linked Lists - LeetCode 206 Added

---
+++ LinkedLists/ReverseLinkedList.java
@@ -0,0 +1,69 @@
+package LinkedLists;
+
+public class ReverseLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode head;
+    public static ListNode tail;
+
+    public void addLast(int val) {
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode nex;
+        while (curr != null) {
+            nex = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nex;
+        }
+        head = prev;
+        return head;
+    }
+
+    public void printLL() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        ReverseLinkedList ll = new ReverseLinkedList();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.printLL();
+        head=ll.reverseList(head);
+        ll.printLL();
+    }
+}
