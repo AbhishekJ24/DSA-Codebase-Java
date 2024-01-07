e181dfe9bfd73b9bd7a82b1137958c795ebfde7f
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-01-07 22:45:58 +0530

    Linked List Cycle - Linked Lists - LeetCode 141 Added

---
+++ LinkedLists/LinkedListCycle.java
@@ -0,0 +1,26 @@
+package LinkedLists;
+
+public class LinkedListCycle {
+    class ListNode {
+        int data;
+        ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        int count = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            count++;
+            if (count > 10000)
+                return true;
+            temp = temp.next;
+        }
+        return false;
+
+    }
+}
