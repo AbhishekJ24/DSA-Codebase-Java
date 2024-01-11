99663d988a4e24870982439f2908849ac1941468
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-01-11 20:49:20 +0530

    Merge Two Sorted Arrays (Brute Force) - Arrays - LeetCode 88 Added

---
+++ Arrays/MergeTwoSortedArray.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class MergeTwoSortedArray{
+    
+    public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i=m,j=0;i<m+n;i++,j++){
+            nums1[i]=nums2[j];
+        }
+        Arrays.sort(nums1);
+        return nums1;
+    }
+    public static void main(String[] args) {
+        int nums1[]={1,2,3,0,0,0};
+        int nums2[]={2,5,6};
+        nums1=merge(nums1,3,nums2,3);
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.print(nums1[i]+" ");
+        }
+    }
+}
