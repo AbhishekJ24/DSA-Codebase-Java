4220112533559abf0404ace56a39415371f7b922
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-02-04 22:05:00 +0530

    Merge Sort Code Added

--- LinkedLists/MergeSort.java
+++ LinkedLists/MergeSort.java
@@ -34,6 +34,59 @@ public class MergeSort {
         System.out.println("null");
     }
 
+    public static Node getMidNode() {
+        Node slowptr = head;
+        Node fastptr = head.next;
+        while (fastptr != null && fastptr.next != null) {
+            slowptr = slowptr.next;
+            fastptr = fastptr.next.next;
+        }
+        return slowptr;
+    }
+
+    public static Node merge(Node left, Node right) {
+        Node mergedLL=new Node(-1);
+        Node temp = mergedLL.next;
+        while (left != null && right != null) {
+            if (left.data <= right.data) {
+                temp.next = left;
+                left = left.next;
+            } else {
+                temp.next = right;
+                right = right.next;
+            }
+            temp = temp.next;
+
+        }
+        while (left != null) {
+            temp.next = left;
+            left = left.next;
+            temp = temp.next;
+        }
+        while (right != null) {
+            temp.next = right;
+            right = right.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+    }
+
+    public Node mergeSort(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        Node midNode = getMidNode();
+
+        Node rightHead = midNode.next;
+        midNode.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+
+        return merge(newLeft, newRight);
+
+    }
+
     public static void main(String[] args) {
         MergeSort ll = new MergeSort();
         ll.addLast(2);
@@ -42,6 +95,8 @@ public class MergeSort {
         ll.addLast(0);
         ll.addLast(2);
         ll.addLast(8);
+        ll.printLL();
+        ll.head = ll.mergeSort(ll.head);
         ll.printLL();
     }
 }
