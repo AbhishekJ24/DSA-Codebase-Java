ef9977a60e1842aa21d8a7a036c0b58e78ebd193
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-02-06 15:27:56 +0530

    Added Min Stack Design - Stacks - LeetCode 155 Added

---
+++ Stacks/MinStack.java
@@ -0,0 +1,38 @@
+package Stacks;
+
+import java.util.*;
+
+class MinStack {
+    ArrayList<Integer> st;
+
+    public MinStack() {
+        st = new ArrayList<Integer>();
+    }
+
+    public void push(int val) {
+        st.add(val);
+    }
+
+    public void pop() {
+        st.remove(st.size() - 1);
+    }
+
+    public int top() {
+        return st.get(st.size() - 1);
+    }
+
+    public int getMin() {
+        ArrayList st1 = (ArrayList) st.clone();
+        Collections.sort(st1);
+        return (int) st1.get(0);
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
