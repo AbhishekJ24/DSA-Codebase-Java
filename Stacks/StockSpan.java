003cfd355d0e79d5384a402e6c3282f8f8e03284
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-03-14 02:20:20 +0530

    Updated stock span

--- Stacks/StockSpan.java
+++ Stacks/StockSpan.java
@@ -1,22 +1,24 @@
 package Stacks;
 
-import java.util.Stack;
+import java.util.*;
 
 public class StockSpan {
     public static void calcSpan(int stocks[], int span[]) {
-        Stack<Integer> st = new Stack<>();
-        span[0] = 1;
+        Stack <Integer> st=new Stack<>();
+        span[0]=1;
         st.push(0);
-        for (int i = 1; i < stocks.length; i++) {
-            while (!st.isEmpty() && stocks[i] >= stocks[st.peek()]) {
+        for(int i=1;i<stocks.length;i++){
+            int curr = stocks[i];
+            while(!st.isEmpty() && curr>stocks[st.peek()]){
                 st.pop();
             }
-            if (st.isEmpty())
-                span[i] = i + 1;
+            if(st.isEmpty())
+            span[i]=i+1;
             else
-                span[i] = i - st.peek();
+            span[i]=i-st.peek();
             st.push(i);
         }
+        
     }
 
     public static void main(String[] args) {
