cbbabd5528fa565257cafbf14e97f8376e99a388
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-03-18 23:54:32 +0530

    Added slightly different approach for valid parentheses using stacks

--- Stacks/ValidParentheses.java
+++ Stacks/ValidParentheses.java
@@ -6,18 +6,37 @@ public class ValidParentheses {
     public static boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
         for (char ch : s.toCharArray()) {
-            if (ch == '(') {
-                st.push(')');
-            } else if (ch == '{') {
-                st.push('}');
-            } else if (ch == '[') {
-                st.push(']');
-            } else if (st.isEmpty() || st.pop()!=ch) {
-                return false;
+            if (ch == '(' || ch == '{' || ch == '[')
+                st.push(ch);
+            else {
+                if (st.isEmpty())
+                    return false;
+                if ((ch == ')' && st.peek() == '(') ||
+                        (ch == '}' && st.peek() == '{') ||
+                        (ch == ']' && st.peek() == '['))
+                    st.pop();
+                else {
+                    return false;
+                }
             }
         }
         return st.isEmpty();
     }
+    // public static boolean isValid(String s) {
+    //     Stack<Character> st = new Stack<>();
+    //     for (char ch : s.toCharArray()) {
+    //         if (ch == '(') {
+    //             st.push(')');
+    //         } else if (ch == '{') {
+    //             st.push('}');
+    //         } else if (ch == '[') {
+    //             st.push(']');
+    //         } else if (st.isEmpty() || st.pop()!=ch) {
+    //             return false;
+    //         }
+    //     }
+    //     return st.isEmpty();
+    // }
     public static void main(String[] args) {
         System.out.println(isValid("[]{(][][][][]{{()}}"));
     }
