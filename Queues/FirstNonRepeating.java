fefe08bff96e86ddf468e65d39065cfa057c961d
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-04-11 17:32:10 +0530

    First Non Repeating Characters in a Stream - Queues added

--- Queues/FirstNonRepeating.java
+++ Queues/FirstNonRepeating.java
@@ -6,11 +6,20 @@ public class FirstNonRepeating {
     public static void firstNonRepeatingCharacter(String s){
         Queue <Character> q = new LinkedList<>();
         int freq[]=new int[26];
-        for(char ch: s.toCharArray()){
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            q.add(ch);
             freq[ch-'a']++;
+            while(!q.isEmpty() && freq[q.peek()-'a']!=1){
+                q.remove();
+            }
+            if(q.isEmpty()){
+                System.out.print("-1  ");
+            }
+            else{
+                System.out.print(q.peek()+"  ");
+            }
         }
-        
-
     }
     public static void main(String[] args) {
         firstNonRepeatingCharacter("aabccxb");
